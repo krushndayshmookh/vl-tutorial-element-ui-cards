24683e03b3f96a9d002381f3b5d50c29bc6a1cb0
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2021-06-09 16:14:06 +0530

    configure tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint",
+    ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
